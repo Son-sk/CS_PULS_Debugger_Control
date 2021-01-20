f5884a3cf6a8dad7620f067cfcc42150ecc3661a
Son-sk <azza2222@naver.com>
2021-01-20 11:04:45 +0900

    210120

---
+++ write.py
@@ -0,0 +1,41 @@
+import time, datetime
+
+def convertBase(number, base): # convert format
+    T="0123456789ABCDEF"
+    i,j=divmod(number,base)
+    if i==0:
+        return T[j]
+    else:
+        return convertBase(i,base)+T[j]
+
+hexdecimal = WatchOption.Hexdecimal
+variable = ["M_SWStep", "M_OpStep", "D_BoilHTset", "D_BoilWTset", "D_A_DATA.OndolTempLimit", "D_RCRTset", "D_RCHTset", "D_RCWTset"]
+variable_hex = ["M_SWStep", "M_OpStep"]
+path = 'C:\Users\jadu900142\Documents\csp_py/new.txt'
+delay = 0.5
+
+f = open(path, 'a')
+i=0
+data = str(datetime.datetime.now())+', '
+for i in variable :
+    data += i+', '
+f.write(data+'\n')
+
+f.close()
+
+while(True) :
+    time.sleep(delay)
+    f = open(path, 'a')
+
+    i=0
+    data = str(datetime.datetime.now())+', '
+    for i in variable :
+        debugger_watch_get = int(debugger.Watch.GetValue(i, hexdecimal))
+        if i in variable_hex :
+            debugger_watch_get_16 = convertBase(debugger_watch_get, 16)
+            data += str(debugger_watch_get_16)+', '   
+        else : 
+            data += str(debugger_watch_get)+', '
+            
+    f.write(data+'\n')
+    f.close()
---
+++ write_set.py
@@ -0,0 +1,2 @@
+debugger.Watch.SetValue("T_ChgSetTemp[1]", 500)
+debugger.Watch.SetValue("C_SetPtr[1]", 20)
